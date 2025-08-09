665bed57da50d7fb70b33674b80170c2e3e1a955
Eugene Kozlov <kozlovea8@gmail.com>
2025-08-09 08:55:39 +0300

    Create spinlock-posix.c

---
+++ c/spinlock-posix.c
@@ -0,0 +1,31 @@
+#include <pthread.h>
+#include <stdio.h>
+
+pthread_spinlock_t lock;
+int counter = 0;
+
+void* worker(void* arg) {
+    for (int i = 0; i < 1000000; i++) {
+        pthread_spin_lock(&lock);   // спин, пока не освободится
+        counter++;
+        pthread_spin_unlock(&lock);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+
+    pthread_spin_init(&lock, PTHREAD_PROCESS_PRIVATE);
+
+    pthread_create(&t1, NULL, worker, NULL);
+    pthread_create(&t2, NULL, worker, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    pthread_spin_destroy(&lock);
+
+    printf("Counter: %d\n", counter);
+    return 0;
+}
